b235e1296bd916f1200ea5a5d4c825b453200136
Minh20812 <blossworld12@gmail.com>
2025-04-04 20:48:16 +0700

    update to deploy

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
-
+export const BASE_URL = "https://server-recall-master.onrender.com";
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const AUTH_URL = `${BASE_URL}/api/auth`;
 export const QUESTIONS_URL = `${BASE_URL}/api/questions`;
